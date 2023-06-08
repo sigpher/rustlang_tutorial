4f6d09e8878920f1a15248ecc3cc1d1f8f5df3c7
sigpher <425223341@qq.com>
2023-06-09 00:36:39 +0800

    finished chapter 09

--- ch09/src/main.rs
+++ ch09/src/main.rs
@@ -1,17 +1,28 @@
 use std::{
-    fs::{self, File},
-    io::{ErrorKind, Write},
+    error::Error,
+    fs::File,
+    io::{self, Read},
 };
 
-fn main() {
-    let mut file = File::open("hello.txt").unwrap_or_else(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            File::create("hello.txt").unwrap_or_else(|error| {
-                panic!("Problem creating the file: {:?}", error);
-            })
-        } else {
-            panic!("Problem opening the file: {:?}", error);
-        }
-    });
-    file.write("hello".as_bytes()).unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    // let mut file = File::open("hello.txt").unwrap_or_else(|error| {
+    //     if error.kind() == ErrorKind::NotFound {
+    //         File::create("hello.txt").unwrap_or_else(|error| {
+    //             panic!("Problem creating the file: {:?}", error);
+    //         })
+    //     } else {
+    //         panic!("Problem opening the file: {:?}", error);
+    //     }
+    // });
+    // file.write("hello".as_bytes()).unwrap();
+    let username = read_username_from_file()?;
+    println!("{username}");
+    Ok(())
+}
+
+pub fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
 }
